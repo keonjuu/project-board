4c5d3a721952b398b9b6732b2dc5200eef7e35e6
keonjuu <inekeon25@gmail.com>
2023-08-18 10:51:41 +0900

    Spring Boot 프로젝트 설정 - JPA Auditing을 활성화

--- src/main/java/exc/Board/BoardApplication.java
+++ src/main/java/exc/Board/BoardApplication.java
@@ -2,7 +2,9 @@ package exc.Board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BoardApplication {
 
