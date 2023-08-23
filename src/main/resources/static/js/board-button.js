2625364cae8fa58f7ae91a06e61b2c6c0137ca9f
keonjuu <inekeon25@gmail.com>
2023-08-23 17:31:32 +0900

    JS 파일 분리 -버튼 save 추가

--- src/main/resources/static/js/board-button.js
+++ src/main/resources/static/js/board-button.js
@@ -23,3 +23,54 @@ function deleteBoard(boardNo){
     formObj.attr("method", "post");
     formObj.submit();
 }
+
+function save(boardNo) {
+    // form data 생성
+    const formData = new FormData();
+
+    const title = $("#title").val();
+    const content = $("#content").val();
+    const filesElement =$("#attachFiles")[0];
+    const changeFilesValue = filesElement.getAttribute("change-files");
+
+    formData.append("title", title);
+    formData.append("content", content);
+    if (changeFilesValue) {
+        if ($("#attachFiles")[0].files.length > 0) {
+            const attachFiles = $("#attachFiles")[0].files;
+            for (let i = 0; i < attachFiles.length; i++) {
+                formData.append("attachFiles", attachFiles[i]);
+                // console.log("attachFiles = ", attachFiles[i]);
+            }
+        }
+    }
+    //const boardNo = [[${board.boardNo}]];
+    $.ajax({
+        enctype: 'multipart/form-data',
+        processData: false,
+        contentType: false,
+        cache: false,
+        type:"POST",
+        url:"/Board/" + boardNo + "/view",
+        data:formData,
+        success:function (data){
+            console.log("data = ", data);
+            alert("수정 사항이 저장되었습니다.");
+
+            $("#saveButton").css("display","none");
+            $("#deleteButton").css("display", "none");
+            $("#editButton").removeAttr("style");
+
+
+            $("input[name='title']").attr("readonly", true);
+            $("textarea[name='content']").attr("readonly", true);
+            $("#attachFiles").css("display", "none");
+            $(".delete-link").hide();
+
+        },
+        error: function(xhr, status, error) {
+            alert(status + "=> 에러났어");
+            console.log(error);
+        }
+    });
+}
